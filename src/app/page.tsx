43bb836287981f05b7e16980f99922679fa6d363
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-12-02 02:13:03 +0500

    update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+
+// work 
+// import Image from "next/image";
+
+// import ParticlesContainer from "../../components/ParticlesContainer";
+import ProjectBtn from "../../components/ProjectsBtn";
+import Avatar from "../../components/Avatar";
+
+// import { motion } from "framer-motion";
+
+// import { fadeIn } from "../variants";
+const Home = () => {
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-80 xl:text-left h-full container mx-auto ">
+          {/* title */}
+          <h1
+            
+            className="h1"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
+            Web Developer <br />
+            <p className="xl:text-xl">Innovative Web Solutions for Modern Businesses</p>{""}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          {/* subtitle */}
+          <p
+            
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            Read our docs
-          </a>
+            As a web developer, I specialize in crafting responsive and user-centered websites, turning ideas into seamless digital experiences. With expertise across front-end and back-end technologies, I build sites that are not only visually engaging but also robust and scalable. My goal is to blend innovative design with functionality to help brands thrive online.
+          </p>
+          <div className="flex justify-center  relative">
+            <ProjectBtn />
+          </div>
+
+          <div
+       
+            className="hidden xl:flex"
+          >,
+            <ProjectBtn />
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
+          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+          {/* <ParticlesContainer /> */}
+
+
+
+          <div
+          
+            className="w-full h-full max-w-[650px] max-h-[600px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] "
+          >
+            <Avatar />
+          </div>
+        </div>
+      </div> 
     </div>
   );
-}
+};
+
+export default Home;
